b4453532ec21113f1c92340cf6ac2bc178b69abe
Cédric Montagne <cmontagne@adlere.fr>
2023-08-31 10:46:03 +0200

    refactor: :recycle: installation Sonarqube via helm chart Bitnami
    fix: :bug: fix import ca from secret
    
    fix: :alien: gitlab token needs expiration date
    
    refactor: :zap: avoid bad response from proxy when vault is down
    
    fix: :bug: keycloak tlsSecret support
    
    refactor: :art: manage tls.type == none
    
    fix: :art: finish variabilize argo
    
    fix: :bug: couldn't remove proxy vars gitlab ci
    
    fix: :art: can provide harbor pvc size
    
    fix: :zap: remove fsGroup and runAsUser values from argocd
    
    fix: :zap: set sonar pg cluster replicas to 2
    
    fix: :bug: fix proxy vars for harbor
    
    fix: :bug: maven_config_file should not be configured in CI

---
+++ filter_plugins/debug.py
@@ -0,0 +1,13 @@
+def get_debug_messages(dsc):
+    messages = []
+    if dsc['proxy']['enabled']:
+        messages.append("--- Proxy ---")
+        messages.append("Nexus Proxy paramaters cannot be set via API, please configure it with local admin account")
+        messages.append("(Parameter Icon) => HTTP => Proxy Settings")
+    return messages
+
+class FilterModule(object):  
+    def filters(self):    
+        return {
+            'get_debug_messages': get_debug_messages,
+        }

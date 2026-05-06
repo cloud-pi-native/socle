f6a2914e156374cf7019ee22fd1ee62e661ba8dc
Stéphane TRÉBEL (Perso) <stephane.trebel@gmail.com>
2026-05-06 14:32:10 +0200

    feat(opencds-mockoon): add deploy script in admin-tools

--- admin-tools/deploy-opencds-mockoon-chart.sh
+++ admin-tools/deploy-opencds-mockoon-chart.sh
@@ -7,25 +7,14 @@ echo "Starting deployment pre-flight checks..."
 echo "----------------------------------------"
 
 APPLICATION="opencds-mockoon"
-CHART_PATH="$GITOPS_REPO_PATH/gitops/envs/conf-dso/apps/$APPLICATION"
-RELEASE_NAME="argocd-$APPLICATION"
+HELM_REPOSITORY="https://cloud-pi-native.github.io/helm-charts"
+HELM_REPOSITORY_ALIAS="cloud-pi-native"
+RELEASE_NAME="cpn-$APPLICATION"
 NAMESPACE="dso-$APPLICATION"
 
-# 1. Directory and File Checks
-
-if [[ ! -d "$CHART_PATH" ]]; then
-    echo "❌ Error: Directory $CHART_PATH does not exist."
-    exit 1
-fi
-
-if [[ ! -f "$CHART_PATH/values.yaml" ]]; then
-    echo "❌ Error: The base 'values.yaml' was not found in $CHART_PATH."
-    exit 1
-fi
-
-# 2. Confirmation prompt
+# 1. Confirmation prompt
 echo ""
-read -p "Do you want to install the dso-$APPLICATION chart? (y/n): " confirm
+read -p "Do you want to install the $RELEASE_NAME chart? (y/n): " confirm
 if [[ "$confirm" != "y" && "$confirm" != "Y" ]]; then
     echo "Installation aborted by administrator."
     exit 0
@@ -35,20 +24,13 @@ echo "Proceeding with installation..."
 echo "-------------------------------"
 
 # 3. Render chart
-helm dependency build "$CHART_PATH"
-helm template "$RELEASE_NAME" "$CHART_PATH" \
-    --namespace "$NAMESPACE" \
-    --create-namespace \
-    --values "$CHART_PATH/values.yaml" > "$FINAL_MANIFEST"
+helm repo add "$HELM_REPOSITORY_ALIAS" "$HELM_REPOSITORY"
+helm repo update
 
-echo "✅ Manifest generated."
-
-# 4. Apply the manifest
-echo "Applying manifest..."
-echo ""
-if kubectl apply -f "$FINAL_MANIFEST" -n "$NAMESPACE" --server-side; then
-    echo "🎉 Deployment of $APPLICATION completed successfully!"
+echo "Installing release..."
+if helm install "$RELEASE_NAME" "$HELM_REPOSITORY_ALIAS/$RELEASE_NAME" --wait --namespace "$NAMESPACE" --create-namespace; then
+    echo "🎉 Release $RELEASE_NAME installed successfully !"
 else
-    echo "❌ Error: kubectl apply failed."
+    echo "❌ Error: helm install failed."
     exit 1
 fi
